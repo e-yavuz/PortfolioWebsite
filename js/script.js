3cc69c66af96f2eeda808f439a357be9f42a6f1a
EmreYavuz45661 <eyavuz45661@gmail.com>
2024-09-28 15:20:33 -0700

    Added read-more button to experience section

--- js/script.js
+++ js/script.js
@@ -45,3 +45,33 @@ function setDynamicPadding() {
 }
 window.addEventListener("load", setDynamicPadding);
 window.addEventListener("resize", setDynamicPadding);
+/* ============================== End of Dynamic Padding ============================ */
+
+/* ============================== "Read More" Project Button ============================ */
+document.addEventListener('DOMContentLoaded', function() {
+    const info = document.querySelectorAll('.project .project-item .project-item-inner .project-info');
+
+    info.forEach(container => {
+        const text_container = container.querySelector('.project-text-container');
+        const text = container.querySelector('.project-text-container .project-text');
+        const ellipsis = container.querySelector('.ellipsis');
+        const readMoreBtn = container.querySelector('.read-more-btn');
+
+        if (text.scrollHeight > text_container.clientHeight) {
+            readMoreBtn.style.display = 'flex'; // Show the button if text overflows
+            ellipsis.style.display = 'flex'; // Show the ellipsis if text
+        }
+
+        readMoreBtn.addEventListener('click', function() {
+            text_container.classList.toggle('expanded');
+            if (text_container.classList.contains('expanded')) {
+                readMoreBtn.textContent = 'Read Less';
+                ellipsis.style.display = 'none'; // Hides ellipsis if text is expanded
+            } else {
+                readMoreBtn.textContent = 'Read More';
+                ellipsis.style.display = 'flex'; // Re-shows ellipsis if text is collapsed
+            }
+        });
+    });
+});
+/* ============================== End of "Read More" Project Button ============================ */
